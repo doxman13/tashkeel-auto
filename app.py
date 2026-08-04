66bb4d53ebab765eedd53b41b4c221f4e1a40e7d
doxman13 <doxman13@gmail.com>
2026-08-04 15:59:42 +0800

    Adjust main container side padding to 4rem

--- app.py
+++ app.py
@@ -1091,8 +1091,8 @@ st.html("""
         section.main .block-container,
         div[data-testid="stAppViewBlockContainer"] {
             max-width: 900px !important;
-            padding-left: 40rem !important;
-            padding-right: 40rem !important;
+            padding-left: 4rem !important;
+            padding-right: 4rem !important;
             margin-left: auto !important;
             margin-right: auto !important;
         }
